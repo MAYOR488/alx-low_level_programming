f56c264cc14d474d5f64c0985683c09116fd649d
mayor488 <adetokunbomayowa@gmail.com>
2023-03-18 13:49:02 -0700

    variable type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ float f;
 printf("Size of char is:%lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of int is:%lu byte(s)\n", (unsigned long)sizeof(i));
 printf("Size of long int is:%lu bytes(s)\n", (unsigned long)sizeof(d));
-printf("Size of long long int is:%lubytes(s)\n", (unsigned long)sizeof(e));
+printf("Size of long long int is:%lu bytes(s)\n", (unsigned long)sizeof(e));
 printf("Size of float is:%lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
