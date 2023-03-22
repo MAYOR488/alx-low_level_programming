5ff4da7e4a86973b2df208db17b2dce650e9d72f
mayor488 <adetokunbomayowa@gmail.com>
2023-03-22 12:13:21 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_alphabet - Entry point
+  * Return: 0 success
+  */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a';  c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
+
