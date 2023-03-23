9cd4c1bca292f268e264958685868731864a55b9
mayor488 <adetokunbomayowa@gmail.com>
2023-03-22 19:23:56 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _isalpha - Entry point
+  * @c: character to be found
+  * Return: 1 if alphabet is lower or uppercase 0 if otherwise
+  */
+
+int _isalpha(int c)
+{
+	if( (c >= 97 && c <= 122) ||( c >= 65 && c <= 90))
+	{
+	return (1);
+	}
+	return (0);
+}
